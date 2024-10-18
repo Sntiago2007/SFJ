4426604c5116c96a86a74c5d5beb97f7c7ea431e
Sntiago2007 <filigramasantiago@gmail.com>
2024-10-18 15:50:09 -0500

    asdfghjkl

---
+++ instructor Henry/lenguajes/Dart/sdfghj.dart
@@ -0,0 +1,3 @@
+int gmvh = 2;
+double abc = 2.2;
+String hola = ('cxcfghj')
--- instructor Henry/lenguajes/Dart/variable y lectura por teclado.dart
+++ instructor Henry/lenguajes/Dart/variable y lectura por teclado.dart
@@ -3,14 +3,14 @@ import 'dart:io';
 void main(){
     
     print("escribe el numero del cual quieres ver su tabla");
-    int N = int.parse(stdin.readLineSync()!);
+    int n = int.parse(stdin.readLineSync()!);
      print(' ');
      print("la tabla del numero $N es:");
      print(' ');
      
     for (var h = 1; h <= 10; h++) {
 
-        int mul = N * h;
+        int mul = n * h;
         print("$N x $h = $mul");
     }
 
